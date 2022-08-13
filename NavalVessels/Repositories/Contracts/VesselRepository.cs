f738ea65cb3636f819766f2ca1011ba2e4eb80ee
Loran Abbas <loranabbas@abv.bg>
2022-08-13 14:51:29 +0300

    VesselRepository complete

---
+++ NavalVessels/Repositories/Contracts/VesselRepository.cs
@@ -0,0 +1,48 @@
+﻿using NavalVessels.Models.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NavalVessels.Repositories.Contracts
+{
+    public class VesselRepository : IRepository<IVessel>
+    {
+        private ICollection<IVessel> vessels;
+        public VesselRepository()
+        {
+            this.vessels = new HashSet<IVessel>();
+        }
+        public IReadOnlyCollection<IVessel> Models
+        {
+            get => (IReadOnlyCollection<IVessel>)this.vessels;
+            private set => this.vessels = (ICollection<IVessel>)value;
+        }
+
+        public void Add(IVessel model)
+        {
+            vessels.Add(model);
+        }
+
+        public IVessel FindByName(string name)
+        {
+            foreach (var vessel in vessels)
+            {
+                if (vessel.Name == name)
+                {
+                    return vessel;
+                }
+            }
+            return null;
+        }
+
+        public bool Remove(IVessel model)
+        {
+            if (vessels.Contains(model))
+            {
+                vessels.Remove(model);
+                return true;
+            }
+            return false;
+        }
+    }
+}
